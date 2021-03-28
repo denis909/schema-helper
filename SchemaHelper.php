492d5392dfa2ff9ed47e35affca5db5a70858c2f
denis909 <denis909@mail.ru>
2021-03-28 20:02:40 +0300

    n/a

---
+++ SchemaHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace denis909\yii;
+
+use Yii;
+
+class SchemaHelper
+{
+
+    public static function createView(string $tableName, string $sql)
+    {
+        $sql = 'CREATE VIEW ' . $tableName . ' AS ' . $sql;
+
+        return Yii::$app->db->createCommand($sql)->execute();
+    }
+
+    public static function dropView(string $tableName)
+    {
+        $sql = 'DROP VIEW ' . $tableName;
+
+        return Yii::$app->db->createCommand($sql)->execute();
+    }
+
+    public static function createTemporaryTable(string $tableName, string $sql)
+    {
+        $sql = 'CREATE TEMPORARY TABLE ' . $tableName . ' ' . $sql;
+
+        return Yii::$app->db->createCommand($sql)->execute();
+    }
+
+    public static function dropTemporaryTable(string $tableName)
+    {
+        $sql = 'DROP TEMPORARY TABLE ' . $tableName;
+
+        return Yii::$app->db->createCommand($sql)->execute();
+    }
+
+}
